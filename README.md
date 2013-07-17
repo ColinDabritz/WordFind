16cb3237f48a3a2ec97b48b348acf357d130773b
Chuck Lauer Vose <vosechu@gmail.com>
2013-07-17 13:50:01 -0700

    added unit tests, trimmed readme. -Colin

--- README.md
+++ README.md
@@ -1,88 +1,28 @@
 This is a ruby word find challenge!
 
-The function is set up to take a puzzle, such as:
+To participate:
+1. fork the repo
+2. clone your fork locally
+3. switch to the first stage branch with `git checkout stage1`
+4. Run gem bundle with `bundle`
+2. implement the puzzle in wordfind.rb.
+3. Check the output manually by running `ruby manualtest.rb`
+    * output should list all words like `BANANA(0,4,E)`
+4. Run (and pass!) unit tests with `rspec wordfind_spec.rb`
+5. Check in your finished code for the stage, push the stage to your fork with `git push origin stage1`
+6. If time allows, repeat these steps for additional stages
 
-```
-L Z K F B D R O W Y E K O J E
-G T H L F S Y L H K A L Y Q N
-L D O A B C L U P A C D R Z I
-I C H Q H B N I Y Q L H A W U
-K Q I B R G R M A W A P R D F
-V F T M A V F S O R S K B F U
-Y A S M A E X P R E S S I O N
-B E R O Q N E H O D T A L M C
-U R A I W R Y J J Y D R N O T
-R P T C A J O D M B G K V Q I
-G C O T H B C I M N E U W N O
-E V O E M D L A G I T H U B N
-M R X F T S L E J Q S A P B U
-G E Y Q K R C I G A M X J U X
-P R S T N C Y K H C O D E G N
-```
+#### rules
 
-as am array of strings and find all the words in a dictionary, such as:
-
-```
-BLOCK
-CLASS
-CODE
-DYNAMIC
-EACH
-EXPRESSION
-FUNCTION
-GEM
-GITHUB
-HASH
-KEYWORD
-LAMBDA
-LIBRARY
-MAGIC
-OPERATOR
-POETRY
-RAILS
-RUBY
-VARIABLE
-```
-
-that are in the puzzle.
-
-* The function returns a list.
-* All eight directions are possible:
-	* N
-	* NE
-	* E
-	* SE
-	* S
-	* SW
-	* W
-	* NW
-* Matching should be case insensitive
-* letters may be shared between words
-
-The puzzle is an array of strings:
-
-```
-abc
-xyz
-lmn
-```
-
-```ruby
-puzzle = ["abc","xyz","lmn"];
-puzzle[row][column] = "l" # letter
-
-# zero indexed!
-
-puzzle[0][0] = "a"
-puzzle[2][1] = "m"
-```
-
-results are an array of strings in the following format:
-* WORD(row,column,direction)
-    * "XM(1,0,SW)"
-* Coordinates are for the first letter of the word, zero based
-* Direction is which way the letters procede from the first
-* word and directions are uppercase.
+* The puzzle is an array of strings (see manual test)
+* The words are an array of strings (see manual test)
+* The output is an array of strings (see results)
+* All eight directions are possible: N, NE, E, SE, S, SW, W, NW
+* Output formatting is:
+    * WORD(first letter row, first letter column,DIRECTION)
+    * "HELLO(1,0,SW)"
+    * Zero based coordinates
 * The results should be in sorted order
+
 
 Puzzle generation courtesy: http://puzzlemaker.discoveryeducation.com/WordSearchSetupForm.asp
--- wordfind_spec.rb
+++ wordfind_spec.rb
@@ -3,62 +3,154 @@ require './wordfind.rb'
 describe "word find" do
   describe "finds words" do
 
+    # Stage 1
   	it "finds simplest word" do
-      puzzle = ["aword"]
-      words = ["word"]
-      find_words(puzzle,words).should eq(["WORD(0,1,E)"])
+      puzzle = ["WORD"]
+      words = ["WORD"]
+      find_words(puzzle,words).should match("WORD")
+    end
+    it "finds words in reverse direction" do
+      puzzle = ["DROW"]
+      words = ["WORD"]
+      find_words(puzzle,words).should match("WORD")
+    end
+    it "finds words forward" do
+      puzzle = ["---WORD---"]
+      words = ["WORD"]
+      find_words(puzzle,words).should match("WORD")
+    end
+    it "finds words backward" do
+      puzzle = ["---DROW---"]
+      words = ["WORD"]
+      find_words(puzzle,words).should match("WORD")
+    end
+    it "finds edge matches" do
+      puzzle = ["WORD---"]
+      words = ["WORD"]
+      find_words(puzzle,words).should match("WORD")
+    end
+    it "finds edge matches" do
+      puzzle = ["---DROW"]
+      words = ["WORD"]
+      find_words(puzzle,words).should match("WORD")
     end
 
-    it "finds words in each direction"
-    it "finds words forward"
-    it "finds words backward"
-    it "finds words south"
-    it "finds words north"
-    it "finds words by boundaries"
+    # Stage 2
+    it "finds words south" do
+      puzzle = %w{W O R D - - -}
+      words = ["WORD"]
+      find_words(puzzle,words).should match("WORD")
+    end
+    it "finds words north" do
+      puzzle = %w{- - - D R O W}
+      words = ["WORD"]
+      find_words(puzzle,words).should match("WORD")
+    end
+    it "finds corner matches" do
+      puzzle = %w{
+        -W
+        -O
+        -R
+        -D
+        --
+        --
+        --}
+      words = ["WORD"]
+      find_words(puzzle,words).should match("WORD")
+    end
+    it "finds words in the middle" do
+      puzzle = %w{
+        -----
+        -----
+        -----
+        --W--
+        --O--
+        --R--
+        --D--
+        -----
+        -----
+        -----}
+      words = ["WORD"]
+      find_words(puzzle,words).should match("WORD")
+    end
+    it "finds single letter matches" do
+      puzzle = ["A"]
+      words = ["A"]
+      find_words(puzzle,words).should match("WORD")
+    end
+    it "finds single letter matches in the middle of other things" do
+      puzzle = %w{
+        ---
+        -A-
+        ---}
+      words = ["A"]
+      find_words(puzzle,words).should match("WORD")
+    end
+
+    # Stage 3
+    describe "correctly identifies matches" do
+      describe "coordinate matches" do
+        it "identifies origin coordinates" do
+          puzzle = ["---WORD---"]
+          words = ["WORD"]
+          find_words(puzzle,words).should match("0,3")
+        end
+        it "identifies first letter coordinates" do
+          puzzle = ["---DROW---"]
+          words = ["WORD"]
+          find_words(puzzle,words).should eq("0,6")
+        end
+        it "identifies single letter coordinates"
+        it "identifies reverse direction coordinates"
+        it "identifies diagonal coordinates"
+        it "identifies final location coordinates"
+        it "identifies single letter final location coordinates"
+        it "identifies single letter origin coordinates"
+      end
+
+      describe "direction matches" do
+        it "identifies north matches" do
+          puzzle = %w{- - - D R O W}
+          words = ["WORD"]
+          find_words(puzzle,words).should match(",N)")
+        end
+        it "identifies south matches" do
+          puzzle = %w{W O R D - - -}
+          words = ["WORD"]
+          find_words(puzzle,words).should match(",S)")
+        end
+        it "identifies east matches" do
+          puzzle = ["---WORD---"]
+          words = ["WORD"]
+          find_words(puzzle,words).should match(",E)")
+        end
+        it "identifies west matches" do
+          puzzle = ["---DROW---"]
+          words = ["WORD"]
+          find_words(puzzle,words).should match(",W)")
+        end
+        it "identifies north-east matches"
+        it "identifies south-east matches"
+        it "identifies south-west matches"
+        it "identifies north-west matches"
+      end
+    end
+
+    # Stage 3
     it "finds words diagonally"
     it "finds words diagonally backward"
-    it "finds edge matches"
-    it "finds corner matches"
+
+    # Stage 4
     it "finds repeated word matches"
     it "finds multiple position matches"
-    it "finds single letter matches"
-    it "finds long words"
-    it "finds max length words"
-  end
-
-  describe "finds word format variations" do
-    it "finds lowercase words in a captial puzzle"
-    it "finds captial words in a lowercase puzzle"
-    it "finds mixed capitalization"
-    it "finds punctuated words"
-  end
-
-  describe "corectly identifies matches" do
-    describe "coordinate matches" do
-      it "identifies origin coordinates"
-      it "identifies first letter coordinates"
-      it "identifies single letter coordinates"
-      it "identifies reverse direction coordinates"
-      it "identifies diagonal coordiantes"
-      it "identifies final location coordinates"
-      it "identifies single letter final location coordinates"
-      it "identifies single letter origin coordinates"
-    end
-
-    describe "direction matches" do
-      it "identifies north matches"
-      it "identifies south matches"
-      it "identifies east matches"
-      it "identifies west matches"
-      it "identifies north-east matches"
-      it "identifies south-east matches"
-      it "identifies south-west matches"
-      it "identifies north-west matches"
-    end
   end
 
   describe "doesn't find" do
-    it "identifies not found words"
+    it "identifies not found words" do
+      puzzle = ["A"]
+      words = ["NOTFOUND"]
+      find_words(puzzle,words).should eq("NOTFOUND()")
+    end
     it "doesn't find wrap-around words"
     it "doesn't find vertical wrap around words"
     it "doesn't find corner wraparound matches"
@@ -68,7 +160,16 @@ describe "word find" do
       # e.g. asdfjkl doesn't match puzzle line dfj"
   end
 
+  describe "finds word format variations" do
+    # Stage 5
+    it "finds lowercase words in a capital puzzle"
+    it "finds capital words in a lowercase puzzle"
+    it "finds mixed capitalization"
+    it "finds punctuated words"
+    it "matches words with overlapping letters"
+  end
+
   describe "output formatting" do
-    it "prints "
+    it "is in sorted order"
   end
 end
